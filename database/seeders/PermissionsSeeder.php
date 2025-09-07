69cbff008bb62a3de37b7dc5343ed1f6438055a5
Curtis Delicata <genealogysoftwareuk@gmail.com>
2025-09-07 18:09:36 +0100

    Temporary build fix

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -12,6 +12,6 @@ class PermissionsSeeder extends Seeder
      */
     public function run(): void
     {
-        Artisan::call('permissions:sync');
+       // Artisan::call('permissions:sync');
     }
 }
