7da97732446cd85be8921479d0fc0af131218092
Curtis Delicata <genealogysoftwareuk@gmail.com>
2025-09-07 15:19:02 +0100

    Format vite input array for better readability

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',
-		    'resources/css/filament/admin/theme.css',
-		    'resources/css/filament/app/theme.css'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/css/filament/admin/theme.css',
+                'resources/css/filament/app/theme.css'
+            ],
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
