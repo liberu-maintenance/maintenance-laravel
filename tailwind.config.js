d5122b6c647c45793eb4b3ba47be9f96eb95ec0f
Curtis Delicata <genealogysoftwareuk@gmail.com>
2025-09-07 15:10:39 +0100

    Rename contact fields to guest fields in work orders

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./app/Filament/**/*.php",
+    "./resources/views/filament/**/*.blade.php",
+    "./vendor/filament/**/*.blade.php",
   ],
   theme: {
     extend: {},
