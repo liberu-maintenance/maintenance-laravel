81d0fb8391c9cce930b2568aab919c3a6b0dcbb9
Curtis Delicata <curtis.delicata@outlook.com>
2025-09-23 23:51:27 +0000

    Revert "Add materials used section and inventory issue logic"
    This reverts commit c8a689293a4dd828d34efa2e670349223d93e354.

--- app/Models/WorkOrder.php
+++ app/Models/WorkOrder.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class WorkOrder extends Model
 {
@@ -58,11 +57,6 @@ class WorkOrder extends Model
     public function team(): BelongsTo
     {
         return $this->belongsTo(Team::class);
-    }
-
-    public function items(): HasMany
-    {
-        return $this->hasMany(WorkOrderItem::class);
     }
 
     public function scopePending($query)
